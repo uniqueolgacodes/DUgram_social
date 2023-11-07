de9dae090512ee7c2ac6b1c9c304928fc79db23d
UniqueOlga <flamezthrower49@gmail.com>
2023-11-07 22:49:40 +0100

    fixed

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -10,11 +10,16 @@ dotenv.config();
 const {AUTH_EMAIL, AUTH_PASSWORD, APP_URL} = process.env;
 
 let transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com", 
+    host: "smtp.office365.com", 
+    port: 587,
+    secure:false,
     auth: {
         user: AUTH_EMAIL,
         pass: AUTH_PASSWORD,    
     },
+    tls: {
+        ciphers: 'SSLv3'
+        }
 });
 
 export const sendVerificationEmail = async (user, res) => {
